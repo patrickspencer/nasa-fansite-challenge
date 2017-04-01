db70d2ff04efa189bd3a31e3381e1dbabc3b94f6
Patrick Spencer <pkspenc@gmail.com>
2017-04-01 18:10:30 -0400

    Add log output dir to settings.py

--- src/settings.py
+++ src/settings.py
@@ -17,6 +17,7 @@ import os
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 log_input_dir = os.path.join(base_dir, 'log_input')
+log_output_dir = os.path.join(base_dir, 'log_output')
 fixture_dir = os.path.join(base_dir, 'insight_testsuite', 'local', 'fixtures')
 
 def load_log_file(file_name):
@@ -24,4 +25,7 @@ def load_log_file(file_name):
 
 def load_fixture(file_name):
     return os.path.join(fixture_dir, file_name)
+
+def log_output_file(file_name):
+    return os.path.join(log_output_dir, file_name)
 
