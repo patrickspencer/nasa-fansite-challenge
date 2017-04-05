db28e13c740c9c163578d92a808bde1b7d79c393
Patrick Spencer <pkspenc@gmail.com>
2017-04-05 19:24:26 -0400

    Update process_log.py for feature4

--- src/process_log.py
+++ src/process_log.py
@@ -48,9 +48,15 @@ pp = pprint.PrettyPrinter(indent=4)
 if __name__ == "__main__":
     start_time = time.time()
 
-    log_file_name = 'log_head_100.txt'
+    print("--- Starting to load file  ---")
+
+    log_file_name = 'log.txt'
     log_file = settings.load_log_file(log_file_name)
     requests = lib.read_file(log_file)
+
+    print("--- Time it took to load file: %s secs ---" % (time.time() - start_time))
+
+    # start_time_1 = time.time()
 
     # print("--- Starting frequency count ---")
     #
@@ -70,14 +76,23 @@ if __name__ == "__main__":
     #
     # print("--- Time to find most demanding resources: %s secs ---" % (time.time() - start_time_2))
 
-    start_time_3 = time.time()
+    # start_time_3 = time.time()
+    #
+    # print("--- Starting failed login attempts finder ---")
+    #
+    # fixture = settings.load_fixture('block_list_test.txt')
+    # requests = lib.read_file(fixture)
+    # lib.write_blocked(lib.check_for_mult_logins(requests))
+    #
+    # print("--- Time to find failed login attempts: %s secs ---" % (time.time() - start_time_3))
 
-    print("--- Starting failed login attempts finder ---")
+    start_time_4 = time.time()
 
-    fixture = settings.load_fixture('block_list_test.txt')
-    requests = lib.read_file(fixture)
-    lib.write_blocked(lib.check_for_mult_logins(requests))
+    print("--- Starting to finding busiest hours ---")
 
-    print("--- Time to find failed login attempts: %s secs ---" % (time.time() - start_time_3))
+    intervals = lib.find_busiest_intervals(requests, time_interval = 3600, n = 10)
+    lib.write_busiest_times(intervals)
+
+    print("--- Time to find failed login attempts: %s secs ---" % (time.time() - start_time_4))
 
     print("--- Total run time: %s seconds ---" % (time.time() - start_time))
