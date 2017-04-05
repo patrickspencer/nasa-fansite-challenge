684cb2c3a3f6e03e394a8be6da01d5c7672fd650
Patrick Spencer <pkspenc@gmail.com>
2017-04-04 21:37:56 -0400

    Update process_log.py for blocked.txt generator

--- src/process_log.py
+++ src/process_log.py
@@ -19,15 +19,38 @@ import time
 import pprint
 import models
 import settings
+from vendor import click
+
+@click.group()
+def feature():
+    pass
+
+@click.command()
+def one():
+    log_file_name = 'log_head_1000.txt'
+    log_file = settings.load_log_file(log_file_name)
+    requests = lib.read_file(log_file)
+
+    start_time = time.time()
+
+    print("--- Starting frequency count ---")
+
+    hosts = lib.create_host_freq_hash(requests)
+    lib.write_file(lib.top_freq(hosts),'hosts.txt')
+
+    print("--- Time to find top frequencies: %s secs ---" % (time.time() - start_time))
+
+feature.add_command(one)
+
 
 pp = pprint.PrettyPrinter(indent=4)
 
 if __name__ == "__main__":
     start_time = time.time()
 
-    # log_file_name = 'log_head_100000.txt'
-    # log_file = settings.load_log_file(log_file_name)
-    # requests = lib.read_file(log_file)
+    log_file_name = 'log_head_100.txt'
+    log_file = settings.load_log_file(log_file_name)
+    requests = lib.read_file(log_file)
 
     # print("--- Starting frequency count ---")
     #
@@ -47,12 +70,14 @@ if __name__ == "__main__":
     #
     # print("--- Time to find most demanding resources: %s secs ---" % (time.time() - start_time_2))
 
+    start_time_3 = time.time()
+
+    print("--- Starting failed login attempts finder ---")
+
     fixture = settings.load_fixture('block_list_test.txt')
     requests = lib.read_file(fixture)
     lib.write_blocked(lib.check_for_mult_logins(requests))
-    #
-    # pp.pprint(block_list)
 
-        # last = int(r.date.timestamp())
+    print("--- Time to find failed login attempts: %s secs ---" % (time.time() - start_time_3))
 
     print("--- Total run time: %s seconds ---" % (time.time() - start_time))
