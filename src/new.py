711a3ac28ee21e75a6ae3c47dffcd8c04f07fa00
Patrick Spencer <pkspenc@gmail.com>
2017-04-05 20:00:48 -0400

    Update new.py

--- src/new.py
+++ src/new.py
@@ -17,39 +17,40 @@ if __name__ == '__main__':
 
     print("--- Time it took to load file: %s secs ---" % (time.time() - start_time))
 
-    # print("--- Starting frequency count ---")
-    #
-    # hosts = lib.create_host_freq_hash(requests)
-    # lib.write_file(lib.top_freq(hosts),'hosts.txt')
-    #
-    # print("--- Time to find top frequencies: %s secs ---" % (time.time() - start_time))
+    print("--- Starting frequency count ---")
 
-    # start_time_2 = time.time()
-    #
-    # print("--- Starting most used resource ---")
-    #
-    # bytes_dict = lib.create_bytes_dict(log_file)
-    # top_bytes = lib.get_top_dict_values(bytes_dict, 10)
-    # pp.pprint(top_bytes)
-    # lib.write_file(top_bytes,'resources.txt', include_values=True)
-    #
-    # print("--- Time to find most demanding resources: %s secs ---" % (time.time() - start_time_2))
+    hosts = lib.create_host_freq_hash(requests)
+    lib.write_file(lib.top_freq(hosts),'hosts.txt')
 
-    # start_time_3 = time.time()
-    #
-    # print("--- Starting failed login attempts finder ---")
-    #
-    # fixture = settings.load_fixture('block_list_test.txt')
-    # requests = lib.read_file(fixture)
-    # lib.write_blocked(lib.check_for_mult_logins(requests))
-    #
-    # print("--- Time to find failed login attempts: %s secs ---" % (time.time() - start_time_3))
+    print("--- Ending frequency count. Time elapsed: %s secs ---" % (time.time() - start_time))
+
+    start_time_2 = time.time()
+
+    print("--- Starting search for most used resources ---")
+
+    bytes_dict = lib.create_bytes_dict(log_file)
+    top_bytes = lib.get_top_dict_values(bytes_dict, 10)
+    lib.write_file(top_bytes,'resources.txt', include_values=True)
+
+    print("--- Ending search for most demanded resources. Time elapsed: %s secs ---" % (time.time() - start_time_2))
+
+    start_time_3 = time.time()
+
+    print("--- Starting search for failed login attempts ---")
+
+    fixture = settings.load_fixture('block_list_test.txt')
+    requests = lib.read_file(fixture)
+    lib.write_blocked(lib.check_for_mult_logins(requests))
+
+    print("--- Ending search for failed login attempts. Time elapsed: %s secs ---" % (time.time() - start_time_3))
 
     start_time_4 = time.time()
 
-    print("--- Starting to finding busiest hours ---")
+    print("--- Starting search for busiest hours ---")
 
-    intervals = lib.find_busiest_intervals(requests, time_interval = 5, n = 11)
+    intervals = lib.find_busiest_intervals(requests, time_interval = 3600, n = 11)
     lib.write_busiest_times(intervals)
 
-    print("--- Time to find failed login attempts: %s secs ---" % (time.time() - start_time_4))
+    print("--- Endign search for busiest hours. Time elapsed: %s secs ---" % (time.time() - start_time_4))
+
+    print("--- Total run time: %s seconds ---" % (time.time() - start_time))
