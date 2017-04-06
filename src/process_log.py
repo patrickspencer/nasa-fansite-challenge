3b1d815f2c01c7058135c8cb4f982879b3f3fefd
Patrick Spencer <pkspenc@gmail.com>
2017-04-05 20:10:35 -0400

    Update process_log.py

--- src/process_log.py
+++ src/process_log.py
@@ -1,125 +1,56 @@
-# -*- coding: utf-8 -*-
-
-"""
-process_log.py
-~~~~~~~~~~~~~~
-Part of the Nasa Fansite Anaslytics Challenge, an Insight Data Engineer
-programming challenge.
-
-process_log.py is the main file for processing logs
-
-:copyright: (c) 2017 by Patrick Spencer.
-"""
-
-import re
+import os
 import lib
 import time
-import pprint
 import models
 import settings
-from vendor import click
+import unittest
+from datetime import datetime
 
-# @click.group()
-# def feature():
-#     pass
-#
-# @click.command()
-# def one():
-#     log_file_name = 'log_head_1000.txt'
-#     log_file = settings.load_log_file(log_file_name)
-#     requests = lib.read_file(log_file)
-#
-#     start_time = time.time()
-#
-#     print("--- Starting frequency count ---")
-#
-#     hosts = lib.create_host_freq_hash(requests)
-#     lib.write_file(lib.top_freq(hosts),'hosts.txt')
-#
-#     print("--- Time to find top frequencies: %s secs ---" % (time.time() - start_time))
+if __name__ == '__main__':
+    start_time = time.time()
 
-# @click.command()
-# def three():
-#     """Find busiest hours"""
-#
-#     start_time = time.time()
-#
-#     print("--- Starting to load file for busiest hour  ---")
-#
-#     log_file_name = 'log_head_100.txt'
-#     log_file = settings.load_log_file(log_file_name)
-#     requests = lib.read_file(log_file)
-#
-#     print("--- Time it took to load file: %s secs ---" % (time.time() - start_time))
-#
-#     start_time_4 = time.time()
-#
-#     print("--- Starting to finding busiest hours ---")
-#
-#     intervals = lib.find_busiest_intervals(requests, time_interval = 3600, n = 10)
-#     print(intervals)
-#     # lib.write_busiest_times(intervals)
-#
-#     print("--- Time to find busiest hours: %s secs ---" % (time.time() - start_time_4))
-#
-#     print("--- Total run time: %s seconds ---" % (time.time() - start_time))
+    print("*** Starting to load file ***\n")
 
-# feature.add_command(one)
-# feature.add_command(three)
-
-if __name__ == "__main__":
-    # feature()
-    # start_time = time.time()
-
-    # print("--- Starting to load file  ---")
-
-    log_file_name = 'log_head_1000.txt'
+    log_file_name = 'log.txt'
     log_file = settings.load_log_file(log_file_name)
     requests = lib.read_file(log_file)
 
-    # print("--- Time it took to load file: %s secs ---" % (time.time() - start_time))
+    print("--- Time it took to load file: %s secs ---" % (time.time() - start_time))
 
-    # start_time_1 = time.time()
+    print("--- Starting frequency count ---")
 
-    # print("--- Starting frequency count ---")
-    #
-    # hosts = lib.create_host_freq_hash(requests)
-    # lib.write_file(lib.top_freq(hosts),'hosts.txt')
-    #
-    # print("--- Time to find top frequencies: %s secs ---" % (time.time() - start_time))
+    hosts = lib.create_host_freq_hash(requests)
+    lib.write_file(lib.top_freq(hosts),'hosts.txt')
 
-    # start_time_2 = time.time()
-    #
-    # print("--- Starting most used resource ---")
-    #
-    # bytes_dict = lib.create_bytes_dict(log_file)
-    # top_bytes = lib.get_top_dict_values(bytes_dict, 10)
-    # pp.pprint(top_bytes)
-    # lib.write_file(top_bytes,'resources.txt', include_values=True)
-    #
-    # print("--- Time to find most demanding resources: %s secs ---" % (time.time() - start_time_2))
+    print("--- Ending frequency count. Time elapsed: %s secs ---" % (time.time() - start_time))
 
-    # start_time_3 = time.time()
-    #
-    # print("--- Starting failed login attempts finder ---")
-    #
-    # fixture = settings.load_fixture('block_list_test.txt')
-    # requests = lib.read_file(fixture)
-    # lib.write_blocked(lib.check_for_mult_logins(requests))
-    #
-    # print("--- Time to find failed login attempts: %s secs ---" % (time.time() - start_time_3))
+    start_time_2 = time.time()
+
+    print("--- Starting search for most used resources ---")
+
+    bytes_dict = lib.create_bytes_dict(log_file)
+    top_bytes = lib.get_top_dict_values(bytes_dict, 10)
+    lib.write_file(top_bytes,'resources.txt', include_values=False)
+
+    print("--- Ending search for most demanded resources. Time elapsed: %s secs ---" % (time.time() - start_time_2))
+
+    start_time_3 = time.time()
+
+    print("--- Starting search for failed login attempts ---")
+
+    fixture = settings.load_fixture('block_list_test.txt')
+    requests = lib.read_file(fixture)
+    lib.write_blocked(lib.check_for_mult_logins(requests))
+
+    print("--- Ending search for failed login attempts. Time elapsed: %s secs ---" % (time.time() - start_time_3))
 
     start_time_4 = time.time()
-    log_file_name = 'log_head_1000.txt'
-    log_file = settings.load_log_file(log_file_name)
-    requests = lib.read_file(log_file)
 
-    print("--- Starting to finding busiest hours ---")
+    print("--- Starting search for busiest hours ---")
 
-    intervals = lib.find_busiest_intervals(requests, time_interval = 3600, n = 10)
-    print(intervals)
-    # lib.write_busiest_times(intervals)
+    intervals = lib.find_busiest_intervals(requests, time_interval = 3600, n = 11)
+    lib.write_busiest_times(intervals)
 
-    print("--- Time to find failed login attempts: %s secs ---" % (time.time() - start_time_4))
-    #
-    # print("--- Total run time: %s seconds ---" % (time.time() - start_time))
+    print("--- Endign search for busiest hours. Time elapsed: %s secs ---" % (time.time() - start_time_4))
+
+    print("--- Total run time: %s seconds ---" % (time.time() - start_time))
